b6f9b33f31fdd06160413382352eb274d8e35e4f
jeffjno <jeff.jno@gmail.com>
2023-08-16 23:42:47 -0300

    Movendo a cobrinha
    Essa atualização permite tanto a movimentação da cobrinha quando inicia o fim do jogo, mas sem essas rotinas, ainda tenho que fazer isso.

--- Assets/Scripts/Manager/SnackGameManager.cs
+++ Assets/Scripts/Manager/SnackGameManager.cs
@@ -1,20 +1,27 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class SnakeGameManager : MonoBehaviour
 {
-    public GameObject blockPrefab; // Referência ao prefab do bloco.
+    public GameObject blockPrefab;
     private Vector2[,] positions;
     private int[,] gameMatrix = new int[10, 20];
 
     private float startX = -1.596f;
-    private float startY =0.211f; // Defina este valor conforme necessário.
+    private float startY = 0.211f;
     private float offset = 0.211f;
+
+    private Snake snake;
+    private Vector2Int applePosition;
+    private Dictionary<Vector2Int, GameObject> boardBlocks = new Dictionary<Vector2Int, GameObject>();
 
     void Start()
     {
         InitializeMatrix();
-        PlaceInitialSnake();
+        InitializeSnake();
         PlaceApple();
+        StartCoroutine(SnakeMovementRoutine());
     }
 
     void InitializeMatrix()
@@ -27,21 +34,96 @@ public class SnakeGameManager : MonoBehaviour
             {
                 positions[x, y] = new Vector2(startX + x * offset, startY + y * offset);
 
-                // Esta parte é opcional, mas se você quiser visualizar a matriz, você pode instanciar os blocos aqui:
-                Instantiate(blockPrefab, positions[x, y], Quaternion.identity).SetActive(true); // Desativado por padrão
+                GameObject block = Instantiate(blockPrefab, positions[x, y], Quaternion.identity);
+                block.SetActive(false); // Desativado por padrão.
+
+                boardBlocks[new Vector2Int(x, y)] = block;
             }
         }
     }
 
-    void PlaceInitialSnake()
+    void InitializeSnake()
     {
-        // Sua lógica para colocar a cobrinha inicialmente no cenário.
+        snake = new Snake(new Vector2Int(5, 10), new Vector2Int(1, 0));
+    }
+
+    void Update()
+    {
+        HandleInput();
+    }
+
+    void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow) && snake.direction.y == 0)
+            snake.direction = new Vector2Int(0, 1);
+        if (Input.GetKeyDown(KeyCode.DownArrow) && snake.direction.y == 0)
+            snake.direction = new Vector2Int(0, -1);
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && snake.direction.x == 0)
+            snake.direction = new Vector2Int(-1, 0);
+        if (Input.GetKeyDown(KeyCode.RightArrow) && snake.direction.x == 0)
+            snake.direction = new Vector2Int(1, 0);
+    }
+
+    IEnumerator SnakeMovementRoutine()
+    {
+        while (true)
+        {
+            snake.Move();
+            CheckCollisions();
+            UpdateGameBoard();
+
+            yield return new WaitForSeconds(0.5f); // Velocidade da cobrinha.
+        }
+    }
+
+    void CheckCollisions()
+    {
+        Vector2Int headPos = snake.segments[0];
+
+        for (int i = 1; i < snake.segments.Count; i++)
+        {
+            if (snake.segments[i] == headPos)
+            {
+                GameOver();
+                return;
+            }
+        }
+
+        if (headPos == applePosition)
+        {
+            snake.Grow();
+            PlaceApple();
+        }
+    }
+
+    void UpdateGameBoard()
+    {
+        foreach (var block in boardBlocks.Values)
+        {
+            block.SetActive(false);
+        }
+
+        foreach (Vector2Int pos in snake.segments)
+        {
+            boardBlocks[pos].SetActive(true);
+        }
+
+        boardBlocks[applePosition].SetActive(true);
     }
 
     void PlaceApple()
     {
-        // lógica para posicionar a maçã de maneira aleatória.
+        do
+        {
+            applePosition = new Vector2Int(Random.Range(0, 10), Random.Range(0, 20));
+        } while (snake.segments.Contains(applePosition));
+
+        boardBlocks[applePosition].SetActive(true);
     }
 
-    // Outros métodos relacionados à lógica do jogo.
+    void GameOver()
+    {
+        StopAllCoroutines();
+        // TODO: Implemente a lógica do GameOver aqui. Você pode mostrar uma mensagem, reiniciar o jogo, etc.
+    }
 }
